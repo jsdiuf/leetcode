0c2931eb96f2ac69764e08c6116cfe0bd2c378ea
jsdiuf <weichun713@foxmail.com>
2018-07-31 09:37:34 +0800

    max water container

--- src/fortest.py
+++ src/fortest.py
@@ -1,22 +1,7 @@
-class Solution(object):
-    def isMatch(self, text, pattern):
-        memo = {}
-        def dp(i, j):
-            if (i, j) not in memo:
-                if j == len(pattern):
-                    ans = i == len(text)
-                else:
-                    first_match = i < len(text) and pattern[j] in {text[i], '.'}
-                    if j+1 < len(pattern) and pattern[j+1] == '*':
-                        ans = dp(i, j+2) or first_match and dp(i+1, j)
-                    else:
-                        ans = first_match and dp(i+1, j+1)
+print(3//2)
+def fun(a):
+    a += 1
+    return a
 
-                memo[i, j] = ans
-            return memo[i, j]
-
-        return dp(0, 0)
-
-
-s=Solution()
-print(s.isMatch("aaaaaaaaaaaaab","a*a*a*a*a*a*a*a*a*a*c"))
+b=2
+print(fun(b))

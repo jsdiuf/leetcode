2aa47dd3b5b9454bacc363d7f8f1683804f4bc57
jsdiuf <weichun713@foxmail.com>
2018-08-08 09:09:58 +0800

    update

--- src/fortest.py
+++ src/fortest.py
@@ -1,5 +1,5 @@
-a=[0,1,2,3]
-print(a[1:-1])
+a=[0,1,2,3,None]
+print(a[0:])
 print(a[-1::-1])
 a=a[::-1]
 print(a)
--- src/lowestCommonAncestor.py
+++ src/lowestCommonAncestor.py
@@ -22,3 +22,63 @@ class Solution(object):
         :type q: TreeNode
         :rtype: TreeNode
         """
+        prelist=[]
+        inlist=[]
+        postlist=[]
+        def traver(node, prelist,inlist):
+
+            prelist.append(node)
+
+            if node.left:
+                traver(node.left, prelist,inlist)
+            inlist.append(node)
+
+            if node.right:
+                traver(node.right, prelist,inlist)
+            postlist.append(node)
+
+        traver(root,prelist,inlist)
+
+        prelist=prelist[0:min(prelist.index(p),prelist.index(q))+1]
+        inlist=inlist[min(inlist.index(p),inlist.index(q)):max(inlist.index(p),inlist.index(q))+1]
+        postlist=postlist[max(postlist.index(p),postlist.index(q)):]
+        inlist=set(prelist).intersection(set(inlist))
+        postlist=set(postlist).intersection(set(inlist))
+
+        return list(postlist)[0]
+
+    def lowestCommonAncestor2(self, root, p, q):
+        if not root:
+            return None
+        if root == p or root == q:
+            return root
+        l = self.lowestCommonAncestor2(root.left, p, q)
+        r = self.lowestCommonAncestor2(root.right, p, q)
+        if l and r:
+            return root
+        return r if l is None else l
+
+n1 = TreeNode(3)
+n2 = TreeNode(5)
+n3 = TreeNode(1)
+n4 = TreeNode(6)
+n5 = TreeNode(2)
+n6 = TreeNode(0)
+n7 = TreeNode(8)
+n8 = TreeNode(7)
+n9 = TreeNode(4)
+
+n1.left = n2
+n1.right = n3
+
+n2.left = n4
+n2.right = n5
+
+n3.left = n6
+n3.right = n7
+
+n5.left = n8
+n5.right = n9
+
+s = Solution()
+print(s.lowestCommonAncestor2(n1, n4, n7).val)

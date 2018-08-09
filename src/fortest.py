6cf26ad507ca57fbacae63db0023209043f47f32
jsdiuf <weichun713@foxmail.com>
2018-08-09 17:55:35 +0800

    update

--- src/fortest.py
+++ src/fortest.py
@@ -1,9 +1,4 @@
-import pickle
-import json
+arr=[2,3,4,6,3,2]
 
-d = [1, 2, 3, 4]
-
-
-def recur(arr, n):
-    return arr[0] if n == 0 else arr[n] + recur(arr, n - 1)
-print(recur(d, len(d) - 1))
+for idx ,val in enumerate(arr):
+    print(idx,val)
